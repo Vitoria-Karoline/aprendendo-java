2e9d4d0d56d44479533acfcfbe1071eb9e1aace8
Vitória Karoline <vitoriakaroline299@gmail.com>
2025-06-01 22:31:03 -0300

    adicionei exercicio com while

--- src/loops/contadorComWhile.java
+++ src/loops/contadorComWhile.java
@@ -1,10 +1,20 @@
 package loops;
 
+import java.util.Scanner;
+
 public class contadorComWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int contadorDeHot = 1;
+		System.out.println("Contando até 50 hot rolls");
+		
+		while(contadorDeHot <= 50 ) {
+			System.out.println(contadorDeHot);
+			contadorDeHot++;
+		}
+		System.out.println("Fim da contagem");
+		System.out.println("Eu amo comida japonesa");
 	}
 
 }
